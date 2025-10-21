50da43b2ddaec819216fbd8f1b17a8b35b306163
Maksim <masxlesfrom@gmail.com>
2025-10-21 19:50:32 +0300

    coords

--- src/components/PrintForm/PrintForm.tsx
+++ src/components/PrintForm/PrintForm.tsx
@@ -119,22 +119,22 @@ const PrintForm: React.FC = () => {
   };
 
   const coords = (data: PrintFormValues) => ({
-    fullName: { x: 460, y: 95 },
-    hunterTicketSeries: { x: 503, y: 115 },
-    hunterTicketNumber: { x: 503, y: 165 },
-    hunterIssueDate: { x: 524, yDay: 120, yMonth: 150, yYear: 255 },
+    fullName: { x: 455, y: 95 },
+    hunterTicketSeries: { x: 498, y: 115 },
+    hunterTicketNumber: { x: 498, y: 165 },
+    hunterIssueDate: { x: 521, yDay: 120, yMonth: 150, yYear: 255 },
     issueDate: { x: 120, yDay: 660, yMonth: 680, yYear: 700 },
-    issuedBy: { x: 37, y: 245 },
-    organizationName: { x: 52, y: 20 },
-    huntingPlace: { x: 351, y: 70 },
-    backIssueDate: { x: 68, yDay: 262, yMonth: 285, yYear: 393 },
+    issuedBy: { x: 31, y: 245 },
+    organizationName: { x: 45, y: 20 },
+    huntingPlace: { x: 346, y: 70 },
+    backIssueDate: { x: 60, yDay: 262, yMonth: 285, yYear: 393 },
     huntType: {x:540, y:55},
     resources: data.resources.map((_, i) => ({
-      resource: { x: 175 + i * 17, y: 40 },
-      dateFrom: { x: 176 + i * 17, y: 143 },
-      dateTo: { x: 176 + i * 17, y: 190 },
-      dailyLimit: { x: 176 + i * 17, y: 250 },
-      seasonLimit: { x: 176 + i * 17, y: 295 },
+      resource: { x: 171 + i * 17, y: 40 },
+      dateFrom: { x: 173 + i * 17, y: 143 },
+      dateTo: { x: 173 + i * 17, y: 190 },
+      dailyLimit: { x: 173 + i * 17, y: 250 },
+      seasonLimit: { x: 173 + i * 17, y: 295 },
     })),
   });
 
